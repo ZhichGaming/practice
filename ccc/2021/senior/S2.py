d90eba44c5534f1ed89ae0901853e4d4f89051f4
ZhichGaming <nick.zhicheng@gmail.com>
2025-01-21 23:50:23 -0500

    2021 s2

--- ccc/2021/senior/S2.py
+++ ccc/2021/senior/S2.py
@@ -2,5 +2,23 @@ M = int(input())
 N = int(input())
 K = int(input())
 
-for i in range(K):
-    
+rows = [False] * (M + 1)
+cols = [False] * (N + 1)
+
+for _ in range(K):
+    direction, value = input().split()
+    value = int(value)
+
+    if direction == "R":
+        rows[value] = not rows[value]
+    else:
+        cols[value] = not cols[value]
+
+result = 0
+
+for i in range(1, M + 1):
+    for j in range(1, N + 1):
+        if (rows[i] or cols[j]) and not (rows[i] and cols[j]):
+            result += 1
+
+print(result)
