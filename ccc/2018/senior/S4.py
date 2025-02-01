82aa3786c6853300a62cf6167757d62092a90433
ZhichGaming <nick.zhicheng@gmail.com>
2025-02-01 02:18:08 -0500

    2018 s4 but 4/5 test cases

--- ccc/2018/senior/S4.py
+++ ccc/2018/senior/S4.py
@@ -3,7 +3,7 @@ N = int(input())
 mem = {}
 
 def getTreeCount(weight):
-    if mem[weight]:
+    if weight in mem:
         return mem[weight]
 
     if weight < 3:
@@ -11,7 +11,17 @@ def getTreeCount(weight):
 
     res = 0
 
-    
+    subtrees = 2
+    each_weight = weight // 2
+
+    while each_weight > 0:
+        # print(subtrees, each_weight)
+        res += getTreeCount(each_weight)
+
+        subtrees += 1
+        each_weight = weight // subtrees
 
     mem[weight] = res
     return res
+
+print(getTreeCount(N))
