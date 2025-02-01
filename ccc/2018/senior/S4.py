820a1887e3b95d8c8facda40681f0c7849726e6a
ZhichGaming <nick.zhicheng@gmail.com>
2025-02-01 02:18:23 -0500

    now only 2 test cases :/

--- ccc/2018/senior/S4.py
+++ ccc/2018/senior/S4.py
@@ -11,15 +11,23 @@ def getTreeCount(weight):
 
     res = 0
 
-    subtrees = 2
+    last_subtree = 1
     each_weight = weight // 2
+    subtrees = weight // each_weight
+    
 
     while each_weight > 0:
         # print(subtrees, each_weight)
-        res += getTreeCount(each_weight)
+        res += getTreeCount(each_weight) * (subtrees - last_subtree)
 
-        subtrees += 1
-        each_weight = weight // subtrees
+        last_subtree = subtrees
+        each_weight -= 1
+
+        if each_weight <= 0:
+            break
+
+        subtrees = weight // each_weight
+        
 
     mem[weight] = res
     return res
